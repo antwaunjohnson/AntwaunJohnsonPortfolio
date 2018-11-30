c1b555c8310276d2dbb1c41a3c703d0453e5d973
antwaunjohnson <antwaun.l.johnson@gmail.com>
2018-11-30 14:45:24 -0700

    Updated enums for blogs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_30_202746) do
+ActiveRecord::Schema.define(version: 2018_11_30_214103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2018_11_30_202746) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
-    t.integer "status"
+    t.integer "status", default: 0
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
   end
 
