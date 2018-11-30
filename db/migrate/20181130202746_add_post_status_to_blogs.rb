a746793fb385a8f0e7390faebab0c6f3a2aa5944
antwaunjohnson <antwaun.l.johnson@gmail.com>
2018-11-30 13:59:22 -0700

    Updated enum for blogs

--- db/migrate/20181130202746_add_post_status_to_blogs.rb
+++ db/migrate/20181130202746_add_post_status_to_blogs.rb
@@ -1,5 +1,5 @@
 class AddPostStatusToBlogs < ActiveRecord::Migration[5.2]
   def change
-    add_column :blogs, :status, :integer, defalut: 0
+    add_column :blogs, :status, :integer, default: 0
   end
 end
