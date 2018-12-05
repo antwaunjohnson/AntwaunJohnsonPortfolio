1ff40560dbe22db8fba51dda62fe451a66d20f76
antwaunjohnson <antwaun.l.johnson@gmail.com>
2018-12-04 17:05:05 -0700

    Refactored before action for portfolio setting

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:edit, :update, :show,  :destroy]
   layout 'portfolio'
 
   def index
@@ -23,12 +24,9 @@ class PortfoliosController < ApplicationController
   end
 
   def edit
-    @portfolio_item = Portfolio.find(params[:id])
   end
 
   def update
-    @portfolio_item = Portfolio.find(params[:id])
-
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'This portfolio item has been successfully updated.' }
@@ -39,12 +37,9 @@ class PortfoliosController < ApplicationController
   end
 
 def show
-  @portfolio_item = Portfolio.find(params[:id])
 end
 
 def destroy
-  @portfolio_item = Portfolio.find(params[:id])
-
   @portfolio_item.destroy
   respond_to do |format|
     format.html { redirect_to portfolis_url, notice: 'Record was obliterated!' }
@@ -60,6 +55,10 @@ def portfolio_params
                                    technologies_attributes: [:name]
                                    )
 
+end
+
+def set_portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])
 end
 
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -6,4 +6,8 @@ class Blog < ApplicationRecord
   validates_presence_of :title, :body
 
   belongs_to :topic, optional: true
+
+  def self.special_blogs
+    all
+  end
 end
