082705a63e9031f8c21cae86db549f7955555c57
antwaunjohnson <antwaun.l.johnson@gmail.com>
2018-12-14 16:42:45 -0700

    Installed jquery ui, sortable html5 library, and css calls

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,8 @@
 //= require popper
 //= require bootstrap
 //= require rails-ujs
+//= require jquery-ui
 //= require activestorage
+//= require html.sortable
 //= require turbolinks
 //= require_tree .
