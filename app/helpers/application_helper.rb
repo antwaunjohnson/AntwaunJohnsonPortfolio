19a07e3e45229b129190e5eb2859b3b17d6d3024
antwaunjohnson <antwaun.l.johnson@gmail.com>
2019-03-01 23:55:59 +0000

    Implemented styles for tech news

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -41,7 +41,12 @@ module ApplicationHelper
       {
         url: portfolios_path,
         title: "Portfolio"
+      },
+      {
+        url: tech_news_path,
+        title: "Tech News"
       }
+      
     ]
   end
 
