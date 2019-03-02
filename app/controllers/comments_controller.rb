5363f3ba5f1d73238c41c610a76a0c838589c053
antwaunjohnson <antwaun.l.johnson@gmail.com>
2019-03-02 22:35:53 +0000

    Refactored form into partial for comments and added validations to comment model

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,9 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = current.user.comments.build(comment_params)
+  end
+  
+  def comment_params
+    params.require(:comment).permit(:comment)
+  end
 end
