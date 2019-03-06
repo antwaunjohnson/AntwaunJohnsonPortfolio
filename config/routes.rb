5b46c81e9b208ceb8dc01cb84243819a42320fed
antwaunjohnson <antwaun.l.johnson@gmail.com>
2019-03-06 00:37:30 +0000

    Created a comment broadcast, configured the broadcast routed, and updated the comment model with a callback

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :comments
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   resources :portfolios, except: [:show] do
     put :sort, on: :collection
@@ -15,7 +14,8 @@ Rails.application.routes.draw do
       get :toggle_status
     end
   end
-
+  
+  mount Actioncable.server => '/cable'
   root to: 'pages#home'
 
 end
