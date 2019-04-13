43a96a4c44d3855dace290d04268de04a7749e72
Antwaun Johnson <antwaun.l.johnson@gmail.com>
2019-04-13 04:37:13 +0000

    Configured production and cable file to incorporate redis cloud

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,8 +43,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://alj-devcamp-portfolio.herokuapp.comcable'
-  config.action_cable.allowed_request_origins = ['http://alj-devcamp-portfolio.herokuapp.com', 'http://alj-devcamp-portfolio.herokuapp.com']
+  config.action_cable.url = 'wss://antwaunjohnson.herokuapp.comcable'
+  config.action_cable.allowed_request_origins = ['http://antwaunjohnson.herokuapp.com', 'http://antwaunjohnson.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
