b1bf1f10d7cc2398d85f864e94c8ec5ddbc5c0b1
Antwaun Johnson <antwaun.l.johnson@gmail.com>
2019-04-13 05:31:17 +0000

    refactored blog side bar for topics

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -19,6 +19,6 @@ class TopicsController < ApplicationController
   private
   
   def set_sidebar_topics
-    @set_sidebar_topics = Topic.with_blogs
+    @side_bar_topics = Topic.with_blogs
   end
 end
